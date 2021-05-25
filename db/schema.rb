a1c8d9089f8cbb68e17d52652861b331392cd987
NakamuraKazki016 <kurookodile553@gmail.com>
2021-05-25 18:28:14 +0000

    Task-App-C1~3 終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210525071317) do
+ActiveRecord::Schema.define(version: 20210525174638) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +29,7 @@ ActiveRecord::Schema.define(version: 20210525071317) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "department"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
